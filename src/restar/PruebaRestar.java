8613e228e79f63ef15781faf281f08c692c30950
Amarico <Amarico@EquipoAcer>
2018-08-20 12:55:21 +0200

    creo el objeto numeroRestado de la clase Resta
    Ejecuta el método ejecutaPrograma

---
+++ src/restar/PruebaRestar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restar;
+
+/**
+ *
+ * @author Amarico
+ */
+public class PruebaRestar {
+    
+    public static void main(String[] args) {
+        
+        //Creo el objeto numeroRestado de la clase Restar
+        Restar numeroRestado = new Restar();
+       
+        // Ejecuta el metodo ejecutaPrograma
+      numeroRestado.ejecutaPrograma();
+        
+        
+    }
+    
+}
